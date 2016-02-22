6d9bf4c388a6fa6a1921fb38bcc8f3bd3037b2ed
beklaham <rjd130330@utdallas.edu>
2016-02-21 22:11:59 -0600

    no node modules

---
+++ App/www/app.js
@@ -0,0 +1,30 @@
+angular.module('App', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('outside', {
+    url: '/outside',
+    abstract: true,
+    templateUrl: 'app/Login/outside.html'
+  })
+  .state('outside.login', {
+    url: '/login',
+    templateUrl: 'app/Login/login.html',
+    controller: 'LoginCtrl'
+  })
+
+  $urlRouterProvider.otherwise('/outside/login');
+})
+
+.run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+  $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
+    if (!AuthService.isAuthenticated()) {
+      console.log(next.name);
+      if (next.name !== 'outside.login') {
+        event.preventDefault();
+        $state.go('outside.login');
+      }
+    }
+  });
+});
---
+++ App/www/app/App/constants.js
@@ -0,0 +1,10 @@
+angular.module('App')
+
+.constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated'
+})
+
+.constant('API_ENDPOINT', {
+  url: 'http://localhost:8080/api'
+  //  For a simulator use: url: 'http://127.0.0.1:8080/api'
+});

e54f561adf8c9f1111a2e850bf6dc1306f6ed2f2
Amy Kiernan <amk130230@utd.edu>
2016-02-24 21:45:54 -0600

    Adding Form folder with service.js which contains functions for API calls

---
+++ App/www/app/Form/services.js
@@ -0,0 +1,40 @@
+angular.module('App')
+
+var getQuestionnaireList = function(patientId) {
+    return $q(function(resolve, reject) {
+    	var endpoint = "patient/v1/patients/" + patientId + "/questionnaires-assigned/";
+      $http.get(API_ENDPOINT.url + endpoint).then(function(result) {
+        if (result.data.success) {
+          resolve(result.data.msg);
+        } else {
+          reject(result.data.msg);
+        }
+      });
+    });
+  };
+
+  var getQuestionnaire = function(patientId, relSurveyPatientId) {
+    return $q(function(resolve, reject) {
+    	var endpoint = "patient/v1/patients/" + patientId + "/questionnaire/" + relSurveyPatientId +"/";
+      $http.get(API_ENDPOINT.url + endpoint).then(function(result) {
+        if (result.data.success) {
+          resolve(result.data.msg);
+        } else {
+          reject(result.data.msg);
+        }
+      });
+    });
+  };
+
+  var submitQuestionnaire = function(questionnaire) {
+    return $q(function(resolve, reject) {
+    	var endpoint = "patient/v1/patients/" + patientId + "/questionnaire/" + relSurveyPatientId +"/";
+      $http.put(API_ENDPOINT.url + endpoint, questionnaire).then(function(result) {
+        if (result.data.success) {
+          resolve(result.data.msg);
+        } else {
+          reject(result.data.msg);
+        }
+      });
+    });
+  };

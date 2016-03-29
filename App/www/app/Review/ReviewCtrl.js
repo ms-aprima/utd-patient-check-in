d0a7545ddca11d7d243107a96e0e235ee2292ed5
beck <rjd130330@utdallas.edu>
2016-03-28 23:25:09 -0400

    testing review view

--- App/www/app/Review/ReviewCtrl.js
+++ App/www/app/Review/ReviewCtrl.js
@@ -1,18 +1,30 @@
 angular.module('App')
  
 .controller('ReviewCtrl', function($scope, FormService, ReviewService, $http, $state) {
+	$scope.sectionsArr = [];
 	$scope.questionnaire = FormService.questionnaire();
 	console.log($scope.questionnaire);
 
-	$scope.submit = function() {
-		console.log("submit in reviewctrl");
-		submitForm();
+
+   $scope.lenn = $scope.questionnaire.Questionnaire.Sections.length;
+
+    for (var i = 0, len = $scope.lenn; i < len; i++) {
+    	$scope.sectionsArr.push($scope.questionnaire.Questionnaire.Sections[i].SurveySectionName);
 	}
+
+
+	
+
+	//$scope.submit = function() {
+		console.log("submit in reviewctrl");
+		//submitForm();
+	//}
 
 	var submitForm = function() {
 		ReviewService.submitQuestionnaire($scope.questionnaire).then(function(result) {
 		    // TODO: reroute to form list
-		    $state.go('inside.form');
+		    console.log("submitted");
+		   // $state.go('inside.form');
 		});
 	};
 

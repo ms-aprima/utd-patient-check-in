19d9089df07cffafa0b58b0fbb0c03ad445aa0e2
beck <rjd130330@utdallas.edu>
2016-03-31 00:44:33 -0400

    UI changes

--- App/www/app/Review/ReviewCtrl.js
+++ App/www/app/Review/ReviewCtrl.js
@@ -5,37 +5,9 @@ angular.module('App')
 	$scope.questions = [];
 	$scope.answers = [];
 	$scope.questionnaire = FormService.questionnaire();
-	console.log($scope.questionnaire);
 
 
-   $scope.lenn = $scope.questionnaire.Questionnaire.Sections.length;
 
-    for (var i = 0;  i < $scope.lenn; i++) {
-    		$scope.sections.push($scope.questionnaire.Questionnaire.Sections[i].SurveySectionName);
-    		$scope.lenn2 = $scope.questionnaire.Questionnaire.Sections[i].Questions.length;
-
-
-			for (var j = 0; j < $scope.lenn2; j++) {	 
-		    	$scope.questions.push($scope.questionnaire.Questionnaire.Sections[i].Questions[j].SurveyQuestionLabel);
-		    	$scope.answers.push($scope.questionnaire.Questionnaire.Sections[i].Questions[j].SelectedAnswerText);
-
-
-			}
-
-	 	}
-	 	console.log($scope.answers);
-	 	console.log($scope.questions);
-
-/*
-	 	for(var i = 0; i< 5; i++)
-	 	{
-	 		console.log(i);
-	 		for(var j=0; j<4 ; j++){
-	 			console.log(j);
-	 		}
-	 	}
-
-	*/
 
 	//$scope.submit = function() {
 		console.log("submit in reviewctrl");

51d28d905235628c652b43456f9eb8d40b220de4
Amy K <amk130230@utd.edu>
2016-04-29 09:49:06 -0500

    No more hardcoded login info

--- App/www/app/Login/LoginCtrl.js
+++ App/www/app/Login/LoginCtrl.js
@@ -2,8 +2,8 @@ angular.module('App')
 
 .controller('LoginCtrl', function($scope, AuthService, $ionicPopup, $state) {
   $scope.user = {
-    Username: 'SHorner',
-    Password: 'f'
+    Username: '',
+    Password: ''
   };
 
   $scope.login = function() {
